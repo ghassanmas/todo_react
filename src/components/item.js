e3cfc93dd54c30ed480178f7d6b5187321475776
Ghassan <ghassan.maslamani@gmail.com>
2018-01-31 11:27:45 +0200

    better view

--- src/components/item.js
+++ src/components/item.js
@@ -1,17 +1,19 @@
 import React from "react";
+import "../App.css";
 
 export class Item extends React.Component {
   render() {
     return (
-      <div>
+      <div className="item">
         <span>{this.props.text}</span>
+        <span>{this.props.priority}</span>
+
         <button
           className="btn-floating btn-large waves-effect waves-light red "
           onClick={this.props.delete}
         >
           <i class="material-icons">delete_forever</i>
         </button>
-        <span>{this.props.priority}</span>
       </div>
     );
   }
